7f723a2f32ba464e24422b59144e4de5a900b2d3
jbreitenbucher <jon@breitenbucher.net>
2014-02-07 14:03:29 -0500

    Wooster Connect Daily initial
    The initial commit of the Connect Daily embed plugin. Calendar does not
    look good when resized. Really only looks good at 800 x 1024.

---
+++ wooster-connectdaily-embed/readme.txt
@@ -0,0 +1,32 @@
+=== Plugin Name ===
+Contributors: kahless
+Donate link: http://orthogonalcreations.com/wooster-connectdaily-embed/donate/
+Tags: connectdaily
+Requires at least: 3.8
+Tested up to: 3.8.1
+Stable tag: 1.0
+
+Add support for auto embedding Connect Daily calendars.
+
+== Description ==
+
+This plugin will allow you to embed a Connect Daily calendar just by pasting the link into your post or page.
+
+== Installation ==
+
+1. Upload the `wooster-connectdaily-embed` folder to the `/wp-content/plugins/` directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+1. Enter your settings in the Wooster Connect Daily options page.
+1. Paste a link to a resource id (e. g., 279) into a post or page
+
+== Frequently Asked Questions ==
+
+== Screenshots ==
+
+1. The Options page.
+2. Player embedded in a post.
+
+== Changelog ==
+
+= 1.0 =
+Initial version
---
+++ wooster-connectdaily-embed/wooster-connectdaily-embed.php
@@ -0,0 +1,100 @@
+<?php
+/*
+Plugin Name: Wooster Connect Daily Embed
+Plugin URI: http://technology.spaces.wooster.edu
+Description: Adds auto embed support to WordPress for Wooster Connect Daily calendars.
+Author: Jon Breitenbucher
+Author URI: http://jon.breitenbucher.net
+Version: 1.0
+*/
+
+/*
+Wooster Connect Daily Embed (Wordpress Plugin)
+Copyright (C) 2011 Jon Breitenbucher
+Contact me at jbreitenbucher@wooster.edu
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// create custom plugin settings menu
+add_action('admin_menu', 'wooster_connectdaily_create_menu');
+
+function wooster_connectdaily_create_menu() {
+// create new menu in the Settings panel
+	add_options_page('Wooster Connect Daily Embed Options', 'Wooster Connect Daily  Embed', 'manage_options', 'woosterconnectdailyembed_settings_page', 'woosterconnectdailyembed_settings_page');
+
+// call register settings function
+	add_action( 'admin_init', 'register_woosterconnectdailyembed_settings' );
+}
+
+
+function register_woosterconnectdailyembed_settings() {
+// register our settings
+	register_setting( 'woosterconnectdailyembed-settings-group', 'woosterconnectdailyembed_calendar_width' );
+	register_setting( 'woosterconnectdailyembed-settings-group', 'woosterconnectdailyembed_calendar_height' );
+}
+
+// create the actual contents of the settings page
+function woosterconnectdailyembed_settings_page() {
+	if (!current_user_can('manage_options'))  {
+		wp_die( __('You do not have sufficient permissions to access this page.') );
+	}
+?>
+<div class="wrap">
+<h2>Wooster Connect Daily Embed Options</h2>
+
+<form method="post" action="options.php">
+    <?php settings_fields( 'woosterconnectdailyembed-settings-group' ); ?>
+    <table class="form-table">
+        <tr valign="top">
+        <th scope="row">Connect Daily Defaults</th>
+        <td>&nbsp;</td>
+        </tr>
+         
+        <tr valign="top">
+        <th scope="row">Default width:</th>
+        <td><input type="text" name="woosterconnectdailyembed_calendar_width" value="<?php echo get_option('woosterconnectdailyembed_calendar_width'); ?>" /> The default width of the Connect Daily calendar.</td>
+        </tr>
+        
+        <tr valign="top">
+        <th scope="row">Default height:</th>
+        <td><input type="text" name="woosterconnectdailyembed_calendar_height" value="<?php echo get_option('woosterconnectdailyembed_calendar_height'); ?>" /> The default height of the Connect Daily calendar.</td>
+        </tr>
+    </table>
+    
+    <p class="submit">
+    <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+    </p>
+
+</form>
+</div>
+<?php } ?>
+<?php
+wp_embed_register_handler( 'connectdaily', '#(\d+)#', 'wp_embed_handler_connectdaily' );
+
+function wp_embed_handler_vconnectdaily( $matches, $attr, $url, $rawattr ) {
+	$width = get_option('woosterconnectdailyembed_calendar_width', '800');
+	$height = get_option('woosterconnectdailyembed_calendar_height', '1024');
+
+	$embed = sprintf(
+			'<iframe frameborder=0 id="calendarframe" scrolling="auto" width=' . esc_attr($width) . ' height=' . esc_attr($height) . 'src="https://connectdaily.wooster.edu/ViewCal.html?dropdown=1&resource_id=%1"> Your browser doesn\'t support frames.
+Click <a href="https://connectdaily.wooster.edu/ViewCal.html?dropdown=1&resource_id=%1">here</a> to view the calendar.
+</IFRAME',
+			esc_attr($matches[1])
+			);
+
+	return apply_filters( 'embed_connectdaily', $embed, $matches, $attr, $url, $rawattr );
+}
+
+?>
